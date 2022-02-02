77297a0072389f8232630ecddc60c16330a3de26
walterg <walterg610@gmail.com>
2022-02-02 09:07:09 -0300

    Lista de productos

---
+++ index.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('#boton').on('click', function(){
+		var enter = $('#entrada').val();
+		if(enter !== ' '){
+
+			var element = $('<li></li>').text(enter);
+			$(element).append("____<button class= 'rem'>X</button>");
+			$('#mostrar').append(element);
+			$('#entrada').val('');
+			$('.rem').on('click', function(){
+				$(this).parent().remove();
+			});
+		}
+		
+	})
+});
